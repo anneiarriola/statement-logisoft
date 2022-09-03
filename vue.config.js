fc230f84aef22f3316e03f805a77d7ef9c0cd592
annieArriola <93558731+annieArriola@users.noreply.github.com>
2022-09-03 16:30:36 -0500

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: ['vuetify', 'vue-tel-input-vuetify'],
-  publicPath: process.env.NODE_ENV === "production" ? "/statement-logisoft/" : "/",
 })
